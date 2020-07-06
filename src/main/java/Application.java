bf0942f9db5a1ba95856a9ba6aca4b68a317eec0
Drelphix <alexeydemeshko@gmail.com>
2020-07-07 00:22:14 +0300

    Search at page

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,7 +14,7 @@ public class Application {
     private static final int depth=8;
 
     public static void main(String[] args) throws IOException {
-        String site;
+
         List<String> terms = new ArrayList<String>();
         List<String> urls = new ArrayList<String>();
         urls.add("https://en.wikipedia.org/wiki/Elon_Musk");
@@ -22,21 +22,41 @@ public class Application {
         terms.add("Musk");
         terms.add("Tesla");
         terms.add("Elon Musk");
-        int[] counts = new int[terms.size()-1];
+        int[] counts = new int[terms.size()];
         Document page = Jsoup.connect(urls.get(0)).get();
         Elements links = page.select("a");
         String absHref = null;
+        int j=0;
+        int test=0;
+            for (String expression : terms) {
+                String text= page.body().text();
+                while (text.indexOf(expression) != -1) {
+                    counts[j]++;
+                    text=text.substring(text.indexOf(expression)+expression.length());
+                }j++;
+            }
+        for (int i = 0; i <counts.length ; i++) {
+            System.out.println(terms.get(i)+" : "+ counts[i]);
+        }
+
         for (Element element : links)
+            if ((!element.attr("href").isEmpty())&&element.attr("href").startsWith("/"))
                 urls.add(element.attr("href"));
             //Поиск
         System.out.println("URLS:");
         for (int i = 1; i < urls.size(); i++) {
-
+            if (urls.get(i).startsWith("//www.")){
+                System.out.println(urls.get(i).substring(2));
+            } else
             System.out.println(urls.get(i));
         }
         Document doc = Jsoup.parse(page.body().text());
         String finded = doc.body().text();
         System.out.println(finded);
+        for (int i = 0; i < counts.length; i++) {
+            System.out.println(counts[i]);
+        }*/
+
 
     }
 
